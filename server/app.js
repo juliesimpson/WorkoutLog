26218c3feb2332bfcb4043a337def0346653acac
juliesimpson <julie.simpson326@gmail.com>
2017-01-30 14:47:20 -0500

    reflecting on previous notes

--- server/app.js
+++ server/app.js
@@ -1,4 +1,12 @@
+// We are importing the express module 
 var express = require("express");
+
+// We are calling the express function 
+// This function puts a new Express application
+// inside the app variable.
+// This is creating an instance of Express that we 
+// can use to call the various Express functions
+// that will help us build our server.
 var app = express();
 
 app.use(require("./middleware/headers"));
@@ -7,6 +15,7 @@ app.use("/api/test", function(req, res) {
 	res.send("Hello World");
 });
 
+// server will start up when it is run on port 3000
 app.listen(3000,function(){
 	console.log("app is listening on 3000");
 });
