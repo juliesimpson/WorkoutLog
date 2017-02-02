223089d79495df5f93bfe09aeec7683b8d76bd24
juliesimpson <julie.simpson326@gmail.com>
2017-02-01 22:39:15 -0500

    attempted logexperiment

---
+++ client/logresult/define.js
@@ -0,0 +1,55 @@
+$(function() {
+	$.extend(WorkoutLog, {
+		definition: {
+			userDefinitions: [],
+
+			create: function() {
+				var defLog = {
+					result: $("#log-result").val(),
+					workout: $("#def-logtype").val(),
+					notes: $("#")
+				};
+
+				var postData = { definition: def };
+				var define = $.ajax({
+					type: "POST",
+					url: WorkoutLog.API_BASE + "definition",
+					data: JSON.stringify(postData),
+					contentType: "application/json"
+				});
+				// .push is pushing info into an array - this will be used
+				// when we log a workout based on category defined.
+				define.done(function(data) {
+					WorkoutLog.definition.userDefinitions.push(data.definition);
+				});
+			},
+			fetchAll: function() {
+				var fetchDefs = $.ajax({
+					type: "GET",
+					url: WorkoutLog.API_BASE + "definition",
+					headers: {
+						"authorization": window.localStorage.getItem("sessionToken")
+					}
+				})
+				.done(function(data) {
+					WorkoutLog.definition.userDefinitions = data;
+				})
+					.fail(function(err) {
+						console.log(err);
+					});
+
+			}
+		}
+	});
+
+	//bindings
+	//this is where we create a workout definition or category
+	$("#def-save").on("click", WorkoutLog.definition.create);
+
+	//fetch definitions if we already are authenticated and refreshed
+	//when there is a sessionToken(authenticated user), grab all workout
+	//definition types or categories
+	if(window.localStorage.getItem("sessionToken")) {
+		WorkoutLog.definition.fetchAll();
+	}
+});
